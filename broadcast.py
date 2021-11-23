9feec99b435a575d4641812c88175594648a56dd
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-11-23 07:29:58 +0530

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -18,3 +18,20 @@ from database import Database
 
 DATABASE_URI = os.environ["DATABASE_URI"]
 db = Database(DATABASE_URI, "FnTelegraphBot")
+
+
+async def send_msg(user_id, message):
+	try:
+		await message.copy(chat_id=user_id)
+		return 200, None
+	except FloodWait as e:
+		await asyncio.sleep(e.x)
+		return send_msg(user_id, message)
+	except InputUserDeactivated:
+		return 400, f"{user_id} : deactivated\n"
+	except UserIsBlocked:
+		return 400, f"{user_id} : user is blocked\n"
+	except PeerIdInvalid:
+		return 400, f"{user_id} : user id invalid\n"
+	except Exception as e:
+		return 500, f"{user_id} : {traceback.format_exc()}\n"
