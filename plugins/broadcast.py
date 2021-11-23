7a17a3398965ef41f6f3731ad34f58984a46c13e
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-11-23 07:37:21 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -14,7 +14,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid, UserNotParticipant, UserBannedInChannel
 from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
 from database import Database
-
+from info import ADMINS
 
 DATABASE_URI = os.environ["DATABASE_URI"]
 db = Database(DATABASE_URI, "FnTelegraphBot")
