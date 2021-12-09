8cee968636bcab7462d5b1cbc88b122522f7e88f
Me <90181293+sinansinu@users.noreply.github.com>
2021-12-09 14:46:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -291,7 +291,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     InlineKeyboardButton('DEVOLEPER', url='https://t.me/sinu_x_07')
                 ]
                 ]
-            await query.message.edit(text="<b>Developer : <a href='https://t.me/sinu_x_07'>This Person</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/HN_MEDIA'>HN-MEDIA</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+            await query.message.edit(text="<b>Developer : <a href='https://t.me/sinu_x_07'>This Person</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/updateschnl'>DH FILM CORNER</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
 
 
